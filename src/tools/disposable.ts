7d723d8759175d7d78dd113eb8241d160f3f79c2
Loïc Mangeonjean <loic@codingame.com>
2022-05-13 11:50:35 +0200

    lib: Import DisposableCollection from monaco-jsonrpc

---
+++ src/tools/disposable.ts
@@ -0,0 +1,24 @@
+import { Disposable } from 'vscode-jsonrpc'
+
+export class DisposableCollection implements Disposable {
+  protected readonly disposables: Disposable[] = []
+
+  dispose (): void {
+    while (this.disposables.length !== 0) {
+      this.disposables.pop()!.dispose()
+    }
+  }
+
+  push (disposable: Disposable): Disposable {
+    const disposables = this.disposables
+    disposables.push(disposable)
+    return {
+      dispose (): void {
+        const index = disposables.indexOf(disposable)
+        if (index !== -1) {
+          disposables.splice(index, 1)
+        }
+      }
+    }
+  }
+}
